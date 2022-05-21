433802b03ea408b1d8badfaa58dfbfc5180d1ef5
anton-karlovskiy <antonkarlovskiy@outlook.com>
2022-05-21 23:36:05 +0300

    chore: clean up

--- ui/pages/index.tsx
+++ ui/pages/index.tsx
@@ -3,12 +3,10 @@ import type { NextPage } from 'next';
 
 import ProviderExample from 'components/ProviderExample';
 import MetaMaskCard from 'components/connector-cards/MetaMaskCard';
-// ray test touch <
 import WalletConnectCard from 'components/connector-cards/WalletConnectCard';
 import CoinbaseWalletCard from 'components/connector-cards/CoinbaseWalletCard';
 import NetworkCard from 'components/connector-cards/NetworkCard';
 import GnosisSafeCard from 'components/connector-cards/GnosisSafeCard';
-// ray test touch >
 
 const Home: NextPage = () => {
   return (
@@ -22,12 +20,10 @@ const Home: NextPage = () => {
           fontFamily: 'sans-serif'
         }}>
         <MetaMaskCard />
-        {/* ray test touch < */}
         <WalletConnectCard />
         <CoinbaseWalletCard />
         <NetworkCard />
         <GnosisSafeCard />
-        {/* ray test touch > */}
       </div>
     </>
   );
