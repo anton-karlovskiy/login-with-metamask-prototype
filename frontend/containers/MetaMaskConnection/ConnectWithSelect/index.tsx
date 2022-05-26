d034e311f03547facaade1712a2905066c219fcf
anton-karlovskiy <antonkarlovskiy@outlook.com>
2022-05-26 06:42:29 +0300

    refactor: clean up ConnectWithSelect

--- frontend/containers/MetaMaskConnection/ConnectWithSelect/index.tsx
+++ frontend/containers/MetaMaskConnection/ConnectWithSelect/index.tsx
@@ -4,43 +4,6 @@ import * as React from 'react';
 import type { Web3ReactHooks } from '@web3-react/core';
 import type { MetaMask } from '@web3-react/metamask';
 
-import {
-  CHAINS,
-  getAddChainParameters
-} from 'config/web3/chains';
-
-const INVALID_CHAIN_ID = -1;
-
-function ChainSelect({
-  chainId,
-  switchChain,
-  displayDefault,
-  chainIds
-}: {
-  chainId: number;
-  switchChain: ((chainId: number) => Promise<void>) | undefined;
-  displayDefault: boolean;
-  chainIds: number[];
-}) {
-  return (
-    <select
-      value={chainId}
-      onChange={event => {
-        switchChain?.(Number(event.target.value));
-      }}
-      disabled={switchChain === undefined}>
-      {displayDefault ? <option value={INVALID_CHAIN_ID}>Default Chain</option> : null}
-      {chainIds.map(chainId => (
-        <option
-          key={chainId}
-          value={chainId}>
-          {CHAINS[chainId]?.name ?? chainId}
-        </option>
-      ))}
-    </select>
-  );
-}
-
 interface Props {
   connector: MetaMask;
   chainId: ReturnType<Web3ReactHooks['useChainId']>;
@@ -56,33 +19,6 @@ function ConnectWithSelect({
   error,
   isActive
 }: Props) {
-  // ray test touch <<
-  const displayDefault = true;
-  // ray test touch >>
-  const chainIds = Object.keys(CHAINS).map(chainId => Number(chainId));
-
-  const [desiredChainId, setDesiredChainId] = React.useState<number>(INVALID_CHAIN_ID);
-
-  const switchChain = React.useCallback(
-    async (desiredChainId: number) => {
-      setDesiredChainId(desiredChainId);
-      // If we're already connected to the desired chain, return
-      if (desiredChainId === chainId) return;
-      // If they want to connect to the default chain and we're already connected, return
-      if (desiredChainId === INVALID_CHAIN_ID && chainId !== undefined) return;
-
-      await connector.activate(
-        desiredChainId === INVALID_CHAIN_ID ?
-          undefined :
-          getAddChainParameters(desiredChainId)
-      );
-    },
-    [
-      connector,
-      chainId
-    ]
-  );
-
   if (error) {
     return (
       <div
@@ -91,23 +27,10 @@ function ConnectWithSelect({
           display: 'flex',
           flexDirection: 'column'
         }}>
-        {/* ray test touch << */}
-        <ChainSelect
-          chainId={desiredChainId}
-          switchChain={switchChain}
-          displayDefault={displayDefault}
-          chainIds={chainIds} />
-        {/* ray test touch >> */}
         {/* TODO: could use tailwindcss */}
         <div style={{ marginBottom: '1rem' }} />
         <button
-          onClick={() => {
-            connector.activate(
-              desiredChainId === INVALID_CHAIN_ID ?
-                undefined :
-                getAddChainParameters(desiredChainId)
-            );
-          }}>
+          onClick={() => void connector.activate(undefined)}>
           Try Again?
         </button>
       </div>
@@ -124,13 +47,6 @@ function ConnectWithSelect({
           display: 'flex',
           flexDirection: 'column'
         }}>
-        {/* ray test touch << */}
-        <ChainSelect
-          chainId={desiredChainId === INVALID_CHAIN_ID ? INVALID_CHAIN_ID : chainId}
-          switchChain={switchChain}
-          displayDefault={displayDefault}
-          chainIds={chainIds} />
-        {/* ray test touch >> */}
         {/* TODO: could use tailwindcss */}
         <div style={{ marginBottom: '1rem' }} />
         <button onClick={() => void connector.deactivate()}>Disconnect</button>
@@ -144,26 +60,13 @@ function ConnectWithSelect({
           display: 'flex',
           flexDirection: 'column'
         }}>
-        {/* ray test touch << */}
-        <ChainSelect
-          chainId={desiredChainId}
-          switchChain={isActivating ? undefined : switchChain}
-          displayDefault={displayDefault}
-          chainIds={chainIds} />
-        {/* ray test touch >> */}
         {/* TODO: could use tailwindcss */}
         <div style={{ marginBottom: '1rem' }} />
         <button
           onClick={
             isActivating ?
               undefined :
-              () => {
-                connector.activate(
-                  desiredChainId === INVALID_CHAIN_ID ?
-                    undefined :
-                    getAddChainParameters(desiredChainId)
-                );
-              }
+              () => void connector.activate(undefined)
           }
           disabled={isActivating}>
           Connect
